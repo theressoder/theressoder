439e35f6018b4566f1f8611285e71e29f9ccd913
Theres <uktsoder@gmail.com>
2025-09-22 20:21:36 +0200

    Remove drizzle and lucia dependencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,7 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		interface Locals {
-			user: import('$lib/server/auth').SessionValidationResult['user'];
-			session: import('$lib/server/auth').SessionValidationResult['session'];
-		}
+		// interface Locals {}
 	} // interface Error {}
 	// interface Locals {}
 } // interface PageData {}
